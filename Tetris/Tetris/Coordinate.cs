2ee3562ffc8698edd8185c331863b3a308fc6ec7
Vlad Golovatyuk <vladd.golovatyuk@gmail.com>
2022-01-14 00:28:10 +0200

    coordinate equality overrided + hashcode

--- Tetris/Tetris/Coordinate.cs
+++ Tetris/Tetris/Coordinate.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tetris
 {
-    internal class Coordinate
+    internal class Coordinate : IEquatable<Coordinate>
     {
         public int X { get; set; }
         public int Y { get; set; }
@@ -20,13 +16,27 @@ namespace Tetris
         {
             return new Coordinate((X + offset.x, Y + offset.y));
         }
-        public override bool Equals(object obj)
+        public void Deconstruct(out int x, out int y)
         {
-            return obj is Coordinate coordinate && coordinate.X == X && coordinate.Y == Y;
+            x = X;
+            y = Y;
         }
         public override string ToString()
         {
             return $"({X}, {Y})";
+        }
+        public bool Equals(Coordinate other)
+        {
+            return other != null &&
+                   X == other.X &&
+                   Y == other.Y;
+        }
+        public override int GetHashCode()
+        {
+            int hashCode = 1861411795;
+            hashCode = hashCode * -1521134295 + X.GetHashCode();
+            hashCode = hashCode * -1521134295 + Y.GetHashCode();
+            return hashCode;
         }
     }
 }
